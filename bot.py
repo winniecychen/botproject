f425e32fc3897b79c53c39ff6ac1825acede630c
winniecychen <cchen18@wellesley.edu>
2019-03-17 12:28:39 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -36,25 +36,23 @@ phrase = 'do you mean the rebels who are TEMPORARILY occuying the legal territor
 #        print(e.reason)
 #    except StopIteration:
 #        break
-
-interval = 30
  
-while True:  
-    for tweet in tweepy.Cursor(api.home_timeline).items(50):
-        try:
-            if tweet.user.screen_name is not 'winniecychen': 
-                text = tweet.text
-                if search in text: 
-                    user_displayname = tweet.user.screen_name
-                    tweet_id = tweet.id
-                    url = 'https://twitter.com/' + str(user_displayname) + '/status/' + str(tweet_id)
-                    api.update_status(phrase + '\n' + url)    
-                    print ("Replied with " + phrase)
-                    time.sleep(interval)
-        except tweepy.TweepError as e:
-            print(e.reason)
-        except StopIteration:
-            break
+#while True:  
+for tweet in tweepy.Cursor(api.home_timeline).items(100):
+    try:
+        if tweet.user.screen_name is not 'winniecychen': 
+            text = tweet.text
+            if search in text: 
+                user_displayname = tweet.user.screen_name
+                tweet_id = tweet.id
+                url = 'https://twitter.com/' + str(user_displayname) + '/status/' + str(tweet_id)
+                api.update_status(phrase + '\n' + url)    
+                print ("Replied with " + phrase)
+                #time.sleep(interval)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
     
 
 
